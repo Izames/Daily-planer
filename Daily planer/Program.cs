d0bf652d7b6ee27e57c81f3799cc0b8af6d70f00
Izames <geday2@mail.ru>
2022-10-20 12:53:44 +0300

    Добавьте файлы проекта.

---
+++ Daily planer/Program.cs
@@ -0,0 +1,77 @@
+﻿using Daily_planer;
+
+namespace MainMenu
+{
+    internal class first_Page
+    {
+        static void Main()
+        {
+            first_Page go = new first_Page();
+            go.zametki();
+        }
+        public void zametki()
+        {
+            Console.Clear();
+            List<string> list = new List<string>()
+            {
+                {"14.10.22" },
+                {" | 15.10.22" },
+                {" | 16.10.22" },
+                {" | 17.10.22" },
+                {" | 18.10.22" },
+            };
+            Console.WriteLine("Добро пожаловать в ежедневник. Выберите то, что хотите сделать");
+            Console.Write(list[0]);
+            Console.Write(list[1]);
+            Console.Write(list[2]);
+            Console.Write(list[3]);
+            Console.WriteLine(list[4]);
+            Console.WriteLine("^");
+            int cursor = 0;
+            ConsoleKeyInfo Clavisha = Console.ReadKey();
+            while (Clavisha.Key != ConsoleKey.Enter)
+            {
+                Console.Clear();
+                Console.WriteLine("Добро пожаловать в ежедневник. Выберите то, что хотите сделать");
+                Console.Write(list[0]);/*0-8*/
+                Console.Write(list[1]);/*10-20*/
+                Console.Write(list[2]);/*21-infinity*/
+                Console.Write(list[3]);/*21-infinity*/
+                Console.Write(list[4]);/*21-infinity*/
+                if (Clavisha.Key == ConsoleKey.RightArrow)
+                    cursor += 11;
+                if (Clavisha.Key == ConsoleKey.LeftArrow)
+                    cursor -= 11;
+                Console.SetCursorPosition(cursor, 2);
+                Console.WriteLine("^");
+                Clavisha = Console.ReadKey();
+            }
+            Console.WriteLine(cursor);
+            if (cursor == 0)
+            {
+                fir Firs = new fir();
+                Firs.one(0);
+            }
+            if (cursor == 11)
+            {
+                fir Firs = new fir();
+                Firs.one(1);
+            }
+            if (cursor == 22)
+            {
+                fir Firs = new fir();
+                Firs.one(2);
+            }
+            if (cursor == 33)
+            {
+                fir Firs = new fir();
+                Firs.one(3);
+            }
+            if (cursor == 44)
+            {
+                fir Firs = new fir();
+                Firs.one(4);
+            }        
+        }
+    }
+}
---
+++ Daily planer/fir.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MainMenu;
+
+namespace Daily_planer
+{
+    internal class fir
+    {
+        public void one(int number)
+        {
+            Console.Clear();
+            List<string[]> list = new List<string[]>()
+            {
+                new string[]{"14.10.22 \n--------\n\t1 часть дня \n\t2 часть дня \n\t3 часть дня \n\t вернуться",
+                    "14.10.22 первая половина дня \n------------------\nв первой половине дня надо убраться в квартире и начать разгребать домашние дела, оставленные на выходные",
+                    "14.10.22 вторая половина дня \n------------------\nво второй половине дня нужно сделать первую из четырех практических",
+                    "14.10.22 третья половина дня \n------------------\nв третьей половине дня можно зайти в дискорд и просто отдохнуть"},
+                new string[]{"15.10.22 \n--------\n\t1 часть дня \n\t2 часть дня \n\t3 часть дня \n\t вернуться",
+                    "14.10.22 первая половина дня \n------------------\nв первой половине дня надо зайти в пекарню и взять себе перекус на утро",
+                    "14.10.22 вторая половина дня \n------------------\nво второй половине дня надо вновь сосредоточиться на практической",
+                    "14.10.22 третья половина дня \n------------------\nв третьей половине дня нужно попрактиковаться в aseprite" },
+                new string[]{"16.10.22 \n--------\n\t1 часть дня \n\t2 часть дня \n\t3 часть дня \n\t вернуться",
+                    "14.10.22 первая половина дня \n------------------\nв первой половине дня надо сходить в аптеку",
+                    "14.10.22 вторая половина дня \n------------------\nво второй половине дня надо начать готовить на неделю",
+                    "14.10.22 третья половина дня \n------------------\nв третьей половине дня можно во что-то поиграть" },
+                new string[]{"17.10.22 \n--------\n\t1 часть дня \n\t2 часть дня \n\t3 часть дня \n\t вернуться",
+                    "14.10.22 первая половина дня \n------------------\nв первой половине дня сдать егэ по борьба",
+                    "14.10.22 вторая половина дня \n------------------\nво второй половине дня надо сходить на тренировку",
+                    "14.10.22 третья половина дня \n------------------\nв третьей половине дня вспомнить о собаке" },
+                new string[]{"17.10.22 \n--------\n\t1 часть дня \n\t2 часть дня \n\t3 часть дня \n\t вернуться",
+                    "14.10.22 первая половина дня \n------------------\nв первой половине дня погулять с собакой",
+                    "14.10.22 вторая половина дня \n------------------\nво второй половине дня слепить снеговика",
+                    "14.10.22 третья половина дня \n------------------\nв третьей половине дня лечь пораньше" },
+            };
+            if (number == 0)
+                txt(list[0]);
+            if (number == 1)
+                txt(list[1]);
+            if (number == 2)
+                txt(list[2]);
+            if (number == 3)
+                txt(list[3]);
+            if (number == 4)
+                txt(list[4]);
+
+        }
+        static void txt(string[] zametka)
+        {
+             Console.WriteLine(zametka[0]);
+             int cursor = 2;
+             Console.SetCursorPosition(2, 2);
+             Console.WriteLine("-->");
+             ConsoleKeyInfo Clavisha = Console.ReadKey();
+             while (Clavisha.Key != ConsoleKey.Enter)
+             {
+                 Console.Clear();
+                 Console.WriteLine(zametka[0]);
+                 if (Clavisha.Key == ConsoleKey.UpArrow)
+                 {
+                     cursor--;
+                 }
+                 if (Clavisha.Key == ConsoleKey.DownArrow)
+                 {
+                     cursor++;
+                 }
+                 Console.SetCursorPosition(0, cursor);
+                 Console.WriteLine("-->");
+                 Clavisha = Console.ReadKey();
+             }
+             if (cursor == 2)
+             {
+                 Console.Clear();
+                 Console.WriteLine(zametka[1]);
+             }
+             if (cursor == 3)
+             {
+                 Console.Clear();
+                 Console.WriteLine(zametka[2]);
+             }
+             if (cursor == 4)
+             {
+                 Console.Clear();
+                 Console.WriteLine(zametka[3]);
+             }
+             if (cursor == 5)
+            {
+                first_Page go = new first_Page();
+                go.zametki();
+            }
+             Console.ReadKey();
+             fir adres = new fir();
+             int tyfytfuyf = 0;
+             adres.one(tyfytfuyf);
+        }
+    }
+}
